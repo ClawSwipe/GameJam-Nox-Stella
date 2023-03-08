16382aaf8e09f67ba02ed9bfffb31366ad7b218d
ClawSwipe <bilalmeghani91@gmail.com>
2023-03-09 03:27:42 +0500

    FINAL BUILD

---
+++ GameJam/Assets/Scripts/CinemachineTest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CinemachineTest : MonoBehaviour
+{
+    public CinemachineFreeLook freeLook;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void update()
+    {
+
+    }
+
+}
